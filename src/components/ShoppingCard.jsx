943512502ad182f7eb2743b5534a2b031b7f5307
QuebecNova <selllmenya@gmail.com>
2022-06-10 15:41:53 +0300

    added overall prices and items icons

--- src/components/ShoppingCard.jsx
+++ src/components/ShoppingCard.jsx
@@ -4,6 +4,17 @@ import ShoppingCardItem from './ShoppingCardItem'
 
 export default function ShoppingCard({shoppingCardItems, handleShoppingCardItemDelete}) {
 
+    let overallPrice = 0;
+
+    shoppingCardItems.forEach(item => {
+        overallPrice += item.price;
+    });
+
+    function displayPrice() {
+        if (!overallPrice) return;
+        return <div className='shopping-card__overall-price'>Price: {overallPrice}$</div>
+    }
+
   return (
     <div className='wrapper__shopping-card'>
         <p>Shopping card</p>
@@ -12,6 +23,7 @@ export default function ShoppingCard({shoppingCardItems, handleShoppingCardItemD
                 return <ShoppingCardItem item={item} key={index} handleShoppingCardItemDelete={handleShoppingCardItemDelete}/>
             })}
         </div>
+        {displayPrice()}
     </div>
   )
 }
